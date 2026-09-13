239ac967bfd40f06e3477f91b152df3ac4a8c537
ruannnnnuri <109990@aluno.uricer.edu.br>
2026-09-13 13:11:58 -0300

    Refactor MainPage layout; improve task tracking logic
    Replaced ScrollView/VerticalStackLayout with Grid in MainPage.xaml for better structure. Moved task list into ScrollView within Grid and positioned task counter at the bottom. In MainPage.xaml.cs, switched to Dictionary<CheckBox, Label> for tracking tasks. Enhanced Add button to validate input and update UI. Updated CheckBox event to modify label style and completed task count.

--- Lista_de_Tarefas/MainPage.xaml.cs
+++ Lista_de_Tarefas/MainPage.xaml.cs
@@ -1,17 +1,59 @@
-﻿namespace Lista_de_Tarefas
+﻿using Microsoft.Maui.Graphics.Text;
+
+namespace Lista_de_Tarefas
 {
     public partial class MainPage : ContentPage
     {
-        
+        Dictionary<CheckBox, Label> list;
+        int tarefasConcluidas;
+        int totalTarefas;
 
         public MainPage()
         {
             InitializeComponent();
+            list = new Dictionary<CheckBox, Label>();
         }
 
-        private void btAdicionar_Clicked(object sender, EventArgs e)
+        private async void btAdicionar_Clicked(object sender, EventArgs e)
         {
+            var newItem = new HorizontalStackLayout();
+            var chkTarefa = new CheckBox();
+            var lbTarefa = new Label { Text = entryTarefa.Text, VerticalOptions = LayoutOptions.Center };
 
+            if (string.IsNullOrWhiteSpace(entryTarefa?.Text))
+            {
+                await DisplayAlertAsync("Erro", "Escreva o nome da tarefa", "OK");
+                return;
+            }
+
+            list[chkTarefa] = lbTarefa;
+
+            newItem.Children.Add(chkTarefa);
+            newItem.Children.Add(lbTarefa);
+
+            listaTarefas.Children.Add(newItem);
+
+            entryTarefa.Text = "";
+
+            chkTarefa.CheckedChanged += OnCheckboxChanged;
+
+            totalTarefas = list.Count();
+
+            lbContagem.Text = $"{tarefasConcluidas} de {totalTarefas} tarefas concluídas";
+        }
+
+        private void OnCheckboxChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var chk = sender as CheckBox;
+
+            Label label = list[chk];
+
+            label.TextDecorations = chk.IsChecked ? TextDecorations.Strikethrough : TextDecorations.None;
+            label.TextColor = chk.IsChecked ? Colors.Grey : Colors.White;
+
+            tarefasConcluidas = list.Keys.Count(chkTarefa => chkTarefa.IsChecked);
+
+            lbContagem.Text = $"{tarefasConcluidas} de {totalTarefas} tarefas concluídas";
         }
     }
 }
